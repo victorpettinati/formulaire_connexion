6e2faf583c2110b94cfcdac7d3f4c8fe7c7da177
Victor Pettinati <victorpettinati@gmail.com>
2023-06-06 17:48:01 +0200

    caroussel 3D finit

--- index.php
+++ index.php
@@ -4,13 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <div class="butt">
-        <a href="connexion.php">Connexion</a>
-    <div class="butt">
-        <a href="inscription.php">Inscription</a>
+
+<h1>Accueil</h1>
+
+<div class="carousel_wrapper">
+  <div class="carousel">
+    <div class="slide one">
+        <a href="inscription.php"><img src="black.jpg" /></a>
+    </div>
+    <div class="slide two">
+        <a href="connexion.php"><img src="black2.jpg" />
+    </div>
+    <div class="slide three">
+        <a href="inscription.php"><img src="black.jpg" /></a>
+    </div>
+    <div class="slide four">
+        <a href="connexion.php"><img src="black2.jpg" />
+    </div>
+<!-- / .carousel -->
+</div><!-- / .carousel_wrapper -->
+
+
     
 </body>
 </html>
---
+++ moduleconnexion.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 06 juin 2023 à 11:08
+-- Version du serveur : 8.0.33
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `moduleconnexion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `login`, `prenom`, `nom`, `password`) VALUES
+(1, 'admin', 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997'),
+(5, 'Thorspliff', 'Victor', 'Pettinati', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220'),
+(7, 'JannetteJP', 'Jean-pierre', 'Jannette', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

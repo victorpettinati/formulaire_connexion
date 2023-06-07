cda60bcb6e76615bbc89fd0d72f0cf25b4f5e29e
Victor Pettinati <victorpettinati@gmail.com>
2023-06-07 13:19:25 +0200

    Levé de rideau

--- admin.php
+++ admin.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Ma page Administrateur</title>
+
+</head>
+<body>
+
+<h1>Administrateur</h1>
+
+<div class="container">                                                                 
+        <div class="text-frame">
 <?php
 $serveur = 'localhost';
 $nomUtilisateur = 'root';
@@ -45,3 +62,10 @@ try {
     echo "Erreur : " . $e->getMessage();
 }
 ?>
+        <a href="deconnexion.php">Déconnexion</a>
+</div>
+</div>
+
+
+</body>
+</html>
